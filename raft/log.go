a9e178f1dabca96a39d3daf3acf7c1833ebe48e8
774889315 <774889315@qq.com>
2020-08-12 08:15:01 -0700

    Project2ac finished

--- raft/log.go
+++ raft/log.go
@@ -78,7 +78,7 @@ func (l *RaftLog) maybeCompact() {
 // unstableEntries return all the unstable entries
 func (l *RaftLog) unstableEntries() []pb.Entry {
 	// Your Code Here (2A).
-	return l.entries[l.stabled : l.LastIndex()]
+	return l.entries[l.stabled:]
 }
 
 // nextEnts returns all the committed but not applied entries
--- raft/rawnode.go
+++ raft/rawnode.go
@@ -75,7 +75,9 @@ type RawNode struct {
 // NewRawNode returns a new RawNode given configuration and a list of raft peers.
 func NewRawNode(config *Config) (*RawNode, error) {
 	// Your Code Here (2A).
-	return nil, nil
+	return &RawNode{
+		Raft: newRaft(config),
+	}, nil
 }
 
 // Tick advances the internal logical clock by a single tick.
@@ -143,19 +145,26 @@ func (rn *RawNode) Step(m pb.Message) error {
 // Ready returns the current point-in-time state of this RawNode.
 func (rn *RawNode) Ready() Ready {
 	// Your Code Here (2A).
-	return Ready{}
+	return Ready{
+		Entries: rn.Raft.RaftLog.unstableEntries(),
+		CommittedEntries: rn.Raft.RaftLog.nextEnts(),
+	}
 }
 
 // HasReady called when RawNode user need to check if any Ready pending.
 func (rn *RawNode) HasReady() bool {
 	// Your Code Here (2A).
-	return false
+	return rn.Raft.RaftLog.LastIndex() > rn.Raft.RaftLog.stabled
 }
 
 // Advance notifies the RawNode that the application has applied and saved progress in the
 // last Ready results.
 func (rn *RawNode) Advance(rd Ready) {
 	// Your Code Here (2A).
+	if len(rd.Entries) > 0 {
+		rn.Raft.RaftLog.stabled = rd.Entries[len(rd.Entries) - 1].Index
+		rn.Raft.RaftLog.applied = rd.Entries[len(rd.Entries) - 1].Index
+	}
 }
 
 // GetProgress return the the Progress of this node and its peers, if this
